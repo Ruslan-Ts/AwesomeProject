babe7b9260abd664ca17d1685e8e758c842c45e4
Ruslan Tsuman <104848743+Ruslan-Ts@users.noreply.github.com>
2023-08-06 12:18:25 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+	TouchableOpacity,
+	SafeAreaView,
+	TextInput,
+	StyleSheet,
+	Text,
+	View,
+	Button,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const signIn = () => {
+		console.debug("Welcome!");
+	};
+	return (
+		<SafeAreaView style={{ flex: 1 }}>
+			<View style={styles.container}>
+				<View style={styles.titleContainer}>
+					<Text style={styles.welcomeTitle}>Welcome to Olx Clone</Text>
+				</View>
+				<View style={styles.formContainer}>
+					<Text style={styles.formTitle}>Sign In</Text>
+					<View>
+						<Text>Email</Text>
+						<TextInput
+							style={styles.input}
+							placeholder="Input your email"
+							autoComplete="email"
+						/>
+					</View>
+					<View>
+						<Text>Password</Text>
+						<TextInput
+							style={styles.input}
+							placeholder="Input your password"
+							autoComplete="password"
+						/>
+					</View>
+					<TouchableOpacity style={styles.button} onPress={signIn}>
+						<Button style={styles.buttonTitle} title="Sign in" />
+					</TouchableOpacity>
+				</View>
+			</View>
+		</SafeAreaView>
+	);
 }
+// 	return (
+// 		<View style={styles.container}>
+// 			<Text style={{ fontSize: 20, margin: 40 }}>Hello to everyone</Text>
+// 			<Button title="click" style={{}} />
+// 			<StatusBar style="auto" />
+// 		</View>
+// 	);
+// }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
